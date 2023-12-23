07df56a85d5b7424626801063332a03d9f618584
Mohith Ganesh <jmnvganesh07@gmail.com>
2023-12-23 12:33:25 +0530

    counter-app added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { useState } from 'react';
+import { render } from 'react-dom';
 import './App.css';
 
-function App() {
+
+
+// state data for 3 counters
+const data = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+  { id: 3, value: 0 }
+];
+
+// totalComponent
+const totalComponent = { total: 0 };
+
+// Counter Component
+function Counter({ id, value, onDecrement, onIncrement }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="counter">
+      <b>{value}</b>
+      <div className="counter-controls">
+        <button
+          className="button-dec"
+          onClick={() => onDecrement(id)}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <button
+          className="button-inc"
+          onClick={() => onIncrement(id)}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
+
+function Total({ counters }) {
+  const total = counters.reduce((acc, counter) => acc + counter.value, 0);
+
+  return (
+    <div className="total-counter">
+      <h1>Total: {total}</h1>
+    </div>
+  );
+}
+
+function App() {
+  const [counters, setCounter] = useState(data);
+
+  const doDecrement = (id) => {
+    setCounter((prevCounters) =>
+      prevCounters.map((counter) =>
+        counter.id === id ? { ...counter, value: counter.value - 1 } : counter
+      )
+    );
+  };
+
+  const doIncrement = (id) => {
+    setCounter((prevCounters) =>
+      prevCounters.map((counter) =>
+        counter.id === id ? { ...counter, value: counter.value + 1 } : counter
+      )
+    );
+  };
+
+  return (
+    <div>
+      {counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          value={counter.value}
+          id={counter.id}
+          onDecrement={doDecrement}
+          onIncrement={doIncrement}
+        />
+      ))}
+      <Total counters={counters} />
+    </div>
+  );
+}
+
+render(<App />, document.querySelector("#root"));
 
 export default App;
